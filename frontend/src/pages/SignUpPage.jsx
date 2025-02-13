f0fbcffe3e52344eda8c88c91c93e8bdf324fcc7
madelineekim <149644675+madelineekim@users.noreply.github.com>
2025-02-12 19:49:34 -0500

    setting up front end paths and configuring stuff

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 const SignUpPage = () => {
+const [showPassword, setShowPassword] = useState(false);
+const [formData, setFormData] = useState({
+  fullName: "",
+  email: "",
+  password: "",
+});
   return (
     <div>
       
