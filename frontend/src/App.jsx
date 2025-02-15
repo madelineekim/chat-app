937c0c8d7e955edc4c585820f9b1f016f5c1d778
madelineekim <149644675+madelineekim@users.noreply.github.com>
2025-02-15 11:12:31 -0500

    sidebar and homepage stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,15 @@ import LogInPage from './pages/LogInPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import {useEffect} from "react";
 import {Loader} from "lucide-react"
 import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -28,7 +31,7 @@ const App = () => {
     );
 
   return (
-    <div >
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
