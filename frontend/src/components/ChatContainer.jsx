1459e5b1be81587f16e4b2c45932bf89bb2eb061
madelineekim <149644675+madelineekim@users.noreply.github.com>
2025-02-16 00:14:57 -0500

    implementing socket

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useRef } from 'react'
 import { useChatStore } from "../store/useChatStore"
 import { useEffect } from 'react'
 import ChatHeader from './ChatHeader'
@@ -8,12 +9,24 @@ import { useAuthStore } from "../store/useAuthStore"
 import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = () => {
-  const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+  const {messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages} = useChatStore();
   const {authUser} = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id)
-  }, [selectedUser._id, getMessages])
+
+    subscribeToMessages();
+
+    return () => unsubscribeFromMessages();
+
+  }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages])
+
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth"})
+    }
+  }, [messages])
 
   if (isMessagesLoading) {
     return (
@@ -25,7 +38,6 @@ const ChatContainer = () => {
     )
   }
 
-
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
@@ -33,7 +45,8 @@ const ChatContainer = () => {
         {messages.map((message) => (
           <div
           key={message._id}
-          className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+          className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+          ref={messageEndRef}>
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
                 <img
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,14 +1,19 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast"
+import { io } from "socket.io-client"
 
-export const useAuthStore = create((set) => ({
+const BASE_URL = "http://localhost:5002";
+
+export const useAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
     onlineUsers: [],
+    socket: null,
+
     checkAuth: async() => {
         try {
             const res = await axiosInstance.get("/auth/check");
@@ -29,6 +34,7 @@ export const useAuthStore = create((set) => ({
           set({ authUser: res.data });
           console.log("Updated authUser in store:", res.data);
           toast.success("Account created successfully");
+          get().connectSocket();
         } catch (error) {
           toast.error(error.response.data.message);
         } finally {
@@ -42,6 +48,8 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/login", data);
             set({ authUser: res.data });
             toast.success("Logged in successfully");
+
+            get().connectSocket();
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
@@ -54,6 +62,7 @@ export const useAuthStore = create((set) => ({
         await axiosInstance.post("/auth/logout");
         set({authUser: null});
         toast.success("Logged out successfully");
+        get().disconnectSocket();
        } catch (error) {
         console.log("Error in logout:", error);
         toast.error(error.response.data.message);
@@ -72,5 +81,25 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isUpdatingProfile: false})
         }
+    },
+
+    connectSocket: () => {
+        const {authUser} = get();
+        if (!authUser || get().socket?.connected) return;
+        const socket = io(BASE_URL, {
+            query: {
+                userId: authUser._id,
+            },  
+        })
+        socket.connect();
+        set({ socket: socket});
+
+        socket.on("getOnlineUsers", (userIds) => {
+            set({ onlineUsers: userIds })
+        })
+    },
+
+    disconnectSocket: () => {
+        if (get().socket?.connected) get().socket.disconnect();
     }
 }));
